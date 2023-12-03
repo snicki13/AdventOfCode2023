3658b5801fe0bce556a3445b406d9352febd70fa
Dominik Kröll <mail@dkroell.de>
2023-12-03 19:32:26 +0100

    Day1_1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,4 +1,7 @@
-﻿namespace AdventOfCode;
+﻿using System.Runtime.InteropServices;
+using Spectre.Console.Rendering;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
@@ -9,7 +12,37 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        char[] digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+        IEnumerable<int> result = [];
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        foreach (var line in _input.Split('\n'))
+        {
+            if (line == "") break;
+            int firstIndex = line.IndexOfAny(digits);
+            int lastIndex = line.LastIndexOfAny(digits);
+            char first = line[firstIndex];
+            char last = line[lastIndex];
+            result = result.Append(int.Parse($"{first}{last}"));
+        }
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1: {result.Sum()}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        char[] digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+        IEnumerable<int> result = [];
+
+        foreach (var line in _input.Split('\n'))
+        {
+            if (line == "") break;
+            int firstIndex = line.IndexOfAny(digits);
+            int lastIndex = line.LastIndexOfAny(digits);
+            char first = line[firstIndex];
+            char last = line[lastIndex];
+            result = result.Append(int.Parse($"{first}{last}"));
+        }
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1: {result.Sum()}");
+    }
 }
