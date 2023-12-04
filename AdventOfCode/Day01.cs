86e474dd19998cdfd3259fe0d4e012957e45d9d6
Dominik Kröll <Dominik.Kroell@mni.thm.de>
2023-12-04 01:25:23 +0100

    Day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -31,18 +31,42 @@ public class Day01 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
+        string[] numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
         char[] digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
         IEnumerable<int> result = [];
 
         foreach (var line in _input.Split('\n'))
         {
-            if (line == "") break;
+            string firstNum = "0";
+            string lastNum = "0";
             int firstIndex = line.IndexOfAny(digits);
+            if (firstIndex != -1)
+            {
+               firstNum = line[firstIndex].ToString();
+            }
             int lastIndex = line.LastIndexOfAny(digits);
-            char first = line[firstIndex];
-            char last = line[lastIndex];
-            result = result.Append(int.Parse($"{first}{last}"));
+            if (lastIndex != -1)
+            {
+               lastNum = line[lastIndex].ToString();
+            }
+            foreach (var num in numbers.Select((x, i) => new { Value = x, Index = i }))
+            {
+                var idx = line.IndexOf(num.Value);
+                var lastIdx = line.LastIndexOf(num.Value);
+                if (idx != -1 && (idx < firstIndex || firstNum == "0"))
+                {
+                    firstIndex = idx;
+                    firstNum = num.Index.ToString();
+                }
+                if (lastIdx > lastIndex)
+                {
+                    lastIndex = lastIdx;
+                    lastNum = num.Index.ToString();
+                }
+            }
+
+            result = result.Append(int.Parse($"{firstNum}{lastNum}"));
         }
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1: {result.Sum()}");
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2: {result.Sum()}");
     }
 }
